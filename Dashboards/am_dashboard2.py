6bec95fc4a7e6d170e10c8b609e5d208b76d221c
ChrisMbeva <chrismbev83@gmail.com>
2022-06-04 14:48:02 +0300

    Update am_dashboard2.py
    modified back country dist to pie

--- Dashboards/am_dashboard2.py
+++ Dashboards/am_dashboard2.py
@@ -327,19 +327,17 @@ country_dist.sort_values('count',ascending=True,inplace=True)
 ########################
 #COUNTRY PIE CHART
 #######################
-country_bar_plot = px.bar(country_dist2,x="count",y='Country',orientation="h",template="plotly_white",color_discrete_sequence=['Blue']*len(country_dist2))
-country_bar_plot.update_layout(
+country_pie = go.Figure(data=[go.Pie(labels=country_dist['Country'], values=country_dist['count'], hole=.3)])
+country_pie.update_layout(
     title={
-        'text': '<b>Coach Count by Country 2021</b>',
-        'y':1.0,
+        'text': "<b>Coaches Country Distribution 2021</b>",
+        'y':0.9,
         'x':0.5,
         'xanchor': 'center',
         'yanchor': 'top'})
 
-country_bar_plot.update_layout(plot_bgcolor="rgba(0,0,0,0)",xaxis=(dict(showgrid=False)))
 
-
-pie2.plotly_chart(country_bar_plot)
+pie2.plotly_chart(country_pie)
 
 
 ## 7.1 Where do they drop off
